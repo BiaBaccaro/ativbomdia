026f8155c8e71ffa91fe49a3a6fc6483af48a245
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-08-11 16:26:53 -0300

    commit bia

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
+
 function saudacaoPorPeriodo() {
-    const agora = new Date();
-    const hora = agora.getHours();
-  
-    let saudacao;
-  
-    if (hora < 12) {
-      saudacao = "Bom dia";
-    } else if (hora < 18) {
-      saudacao = "Boa tarde";
-    } else {
-      saudacao = "Boa noite";
+      const agora = new Date();
+      const hora = agora.getHours();
+
+      let saudacao;
+
+      if (hora < 12) {
+        saudacao = "Bom dia";
+      } else if (hora < 18) {
+        saudacao = "Boa tarde";
+      } else {
+        saudacao = "Boa noite";
+      }
+
+      return saudacao;
+    }
+
+    function exibirSaudacao() {
+      const saudacao = saudacaoPorPeriodo();
+      document.getElementById("mensagem").textContent = saudacao;
     }
   
-    return saudacao;
-  }
-  
-  const saudacao = saudacaoPorPeriodo();
-  console.log(saudacao);
   
   
